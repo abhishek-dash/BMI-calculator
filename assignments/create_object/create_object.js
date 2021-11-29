97ee7f49863f8cb266ad5be7e9295d6af0052835
Abhi <abhishek953dash@gmail.com>
2021-11-29 19:43:21 +0530

    Created object

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,10 @@
 function CreateObject(arr) {
     // Write your code here
+    let info={};
+    for(var i=0;i<arr.length;i+=2){
+        info[arr[i]]=arr[i+1]
+    }
+    return info
 }
 
 module.exports = CreateObject;
